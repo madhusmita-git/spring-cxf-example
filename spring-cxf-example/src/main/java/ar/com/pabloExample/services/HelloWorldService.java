5b61657a991275f8ae40498625d8bac9949fb327
madhusmitap <madhusmitap@madhusmitap7>
2014-01-31 14:25:12 +0530

    Enable default logging in ApacheCXF

--- spring-cxf-example/src/main/java/ar/com/pabloExample/services/HelloWorldService.java
+++ spring-cxf-example/src/main/java/ar/com/pabloExample/services/HelloWorldService.java
@@ -5,11 +5,13 @@ import javax.jws.WebService;
 
 import org.apache.cxf.annotations.Logging;
 import org.apache.cxf.feature.Features;
-import org.apache.cxf.interceptor.InInterceptors;
-import org.apache.cxf.interceptor.OutInterceptors;
 
 @WebService
 @Features(features="org.apache.cxf.feature.LoggingFeature")
+@Logging(pretty=true, 
+	inLocation="file:/C:/wsimport/log.txt", 
+	outLocation="file:/C:/wsimport/log.txt"
+)
 public interface HelloWorldService {
 	
 	public String sayHello(@WebParam(name="parameter") String name);
