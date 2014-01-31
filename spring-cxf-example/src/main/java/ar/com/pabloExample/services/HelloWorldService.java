d0198cb632ee7e38074a5b6f4516a2dbb8bbe38d
madhusmitap <madhusmitap@madhusmitap7>
2014-01-31 14:17:29 +0530

    Taken-off un-necessary commented blocks

--- spring-cxf-example/src/main/java/ar/com/pabloExample/services/HelloWorldService.java
+++ spring-cxf-example/src/main/java/ar/com/pabloExample/services/HelloWorldService.java
@@ -10,13 +10,6 @@ import org.apache.cxf.interceptor.OutInterceptors;
 
 @WebService
 @Features(features="org.apache.cxf.feature.LoggingFeature")
-/*
- * Used for ApacheCXF default logging using j2se common logging
- * 
- * @Logging(pretty=true, 
-	inLocation="file:/C:/wsimport/log.txt", 
-	outLocation="file:/C:/wsimport/log.txt"
-	)*/
 public interface HelloWorldService {
 	
 	public String sayHello(@WebParam(name="parameter") String name);
