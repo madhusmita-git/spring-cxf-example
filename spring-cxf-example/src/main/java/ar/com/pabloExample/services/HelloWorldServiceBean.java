92ec6bc942fef579faf86b1d2c5883c5b6583374
madhusmitap <madhusmitap@madhusmitap7>
2014-01-31 16:37:27 +0530

    Added some logger from Java file with-in method

--- spring-cxf-example/src/main/java/ar/com/pabloExample/services/HelloWorldServiceBean.java
+++ spring-cxf-example/src/main/java/ar/com/pabloExample/services/HelloWorldServiceBean.java
@@ -1,30 +1,24 @@
 package ar.com.pabloExample.services;
 
-import java.util.Properties;
-import java.util.Set;
-
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.apache.log4j.Logger;
+
 @WebService(endpointInterface = "ar.com.pabloExample.services.HelloWorldService")
 public class HelloWorldServiceBean implements HelloWorldService{
-
+	private static final Logger log = Logger.getLogger(HelloWorldServiceBean.class);
+	
 	@Override
 	public String sayHello(String name) {
-		System.out.println("Name: " + name);
+		log.info("sayHello(String name)");
 		return name.toUpperCase() + " - Hello!!!";
 	}
 
 	@WebMethod
 	@Override
 	public String helloWorld() {
-		
-        Properties properties = System.getProperties();
-        Set<Object>  sysPropertiesKeys = properties.keySet();
-        for (Object key : sysPropertiesKeys) {
-            System.out.println(key + " =" + properties.getProperty((String)key));
-        }		
-		
+		log.info("helloWorld()");
 		return "Hello World" + System.getProperty("org.apache.cxf.Logger") + " -> " + System.getProperty("java.version");
 	}
 }
